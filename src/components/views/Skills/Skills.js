571f035c54a12558923eab819a3926804b1da58f
Karol Sobolewski <karolsobolewski92@gmail.com>
2021-02-17 16:37:13 +0100

    Fill skills section

--- src/components/views/Skills/Skills.js
+++ src/components/views/Skills/Skills.js
@@ -1,6 +1,28 @@
 import React, { useState, useEffect } from 'react';
 import { useSelector } from 'react-redux';
 // import { useDispatch } from 'react-redux';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPencilRuler } from '@fortawesome/free-solid-svg-icons';
+import {
+  faHtml5,
+  faCss3Alt,
+  faJsSquare,
+  faReact,
+  faBootstrap,
+  faWordpressSimple,
+  faNodeJs,
+} from '@fortawesome/free-brands-svg-icons';
+import { Icon, InlineIcon } from '@iconify/react';
+import reduxIcon from '@iconify-icons/simple-icons/redux';
+import expressIcon from '@iconify-icons/simple-icons/express';
+import mongodbIcon from '@iconify-icons/simple-icons/mongodb';
+import websocketIcon from '@iconify-icons/logos/websocket';
+import jiraIcon from '@iconify-icons/simple-icons/jira';
+import figmaIcon from '@iconify-icons/simple-icons/figma';
+import adobephotoshopIcon from '@iconify-icons/simple-icons/adobephotoshop';
+import adobeillustratorIcon from '@iconify-icons/simple-icons/adobeillustrator';
+import writeDotAs from '@iconify-icons/simple-icons/write-dot-as';
+
 import PropTypes from 'prop-types';
 import clsx from 'clsx';
 
@@ -20,47 +42,86 @@ const Component = ({ className, children }) => {
     <div className={clsx(className, styles.root)}>
       <h2>Languages</h2>
       <Row className={styles.skillRow}>
-        <Col className="col-12 col-md-6 col-lg-4 d-flex justify-content-center align-items-center p-0 mt-3 mb-3">
-          HTML
+        <Col className={`${styles.skillCol} col-12 col-md-6 col-lg-4`}>
+          <div className={styles.skillIcon}>
+            <FontAwesomeIcon icon={faHtml5} />
+          </div>
+          <p>HTML</p>
         </Col>
-        <Col className="col-12 col-md-6 col-lg-4 d-flex justify-content-center align-items-center p-0 mt-3 mb-3">
-          CSS
+        <Col className={`${styles.skillCol} col-12 col-md-6 col-lg-4`}>
+          <div className={styles.skillIcon}>
+            <FontAwesomeIcon icon={faCss3Alt} />
+          </div>
+          <p>CSS</p>
         </Col>
-        <Col className="col-12 col-md-6 col-lg-4 d-flex justify-content-center align-items-center p-0 mt-3 mb-3">
-          JavaScript
+        <Col className={`${styles.skillCol} col-12 col-md-6 col-lg-4`}>
+          <div className={styles.skillIcon}>
+            <FontAwesomeIcon icon={faJsSquare} />
+          </div>
+          <p>JavaScript</p>
         </Col>
       </Row>
       <h2>Tools</h2>
       <Row className={styles.skillRow}>
-        <Col className="col-12 col-md-6 col-lg-4 d-flex justify-content-center align-items-center p-0 mt-3 mb-3">
-          React with hooks
+        <Col className={`${styles.skillCol} col-12 col-md-6 col-lg-4`}>
+          <div className={styles.skillIcon}>
+            <FontAwesomeIcon icon={faReact} />
+          </div>
+          <p>React with hooks</p>
         </Col>
-        <Col className="col-12 col-md-6 col-lg-4 d-flex justify-content-center align-items-center p-0 mt-3 mb-3">
-          Redux
+        <Col className={`${styles.skillCol} col-12 col-md-6 col-lg-4`}>
+          <div className={styles.skillIcon}>
+            <Icon icon={reduxIcon} className={styles.skillIcon} />
+          </div>
+          <p>Redux</p>
         </Col>
-        <Col className="col-12 col-md-6 col-lg-4 d-flex justify-content-center align-items-center p-0 mt-3 mb-3">
-          Bootstrap
+        <Col className={`${styles.skillCol} col-12 col-md-6 col-lg-4`}>
+          <div className={styles.skillIcon}>
+            <FontAwesomeIcon icon={faBootstrap} />
+          </div>
+          <p>Bootstrap</p>
         </Col>
-        <Col className="col-12 col-md-6 col-lg-4 d-flex justify-content-center align-items-center p-0 mt-3 mb-3">
-          WordPress
+        <Col className={`${styles.skillCol} col-12 col-md-6 col-lg-4`}>
+          <div className={styles.skillIcon}>
+            <FontAwesomeIcon icon={faWordpressSimple} />
+          </div>
+          <p>WordPress</p>
         </Col>
-        <Col className="col-12 col-md-6 col-lg-4 d-flex justify-content-center align-items-center p-0 mt-3 mb-3">
-          Node.js
+        <Col className={`${styles.skillCol} col-12 col-md-6 col-lg-4`}>
+          <div className={styles.skillIcon}>
+            <FontAwesomeIcon icon={faNodeJs} />
+          </div>
+          <p>Node.js</p>
         </Col>
-        <Col className="col-12 col-md-6 col-lg-4 d-flex justify-content-center align-items-center p-0 mt-3 mb-3">
-          Express
+        <Col className={`${styles.skillCol} col-12 col-md-6 col-lg-4`}>
+          <div className={styles.skillIcon}>
+            <Icon icon={expressIcon} />
+          </div>
+          <p>Express</p>
         </Col>
-        <Col className="col-12 col-md-6 col-lg-4 d-flex justify-content-center align-items-center p-0 mt-3 mb-3">
-          MongoDB
+        <Col className={`${styles.skillCol} col-12 col-md-6 col-lg-4`}>
+          <div className={styles.skillIcon}>
+            <Icon icon={mongodbIcon} className={styles.skillIcon} />
+          </div>
+          <p>MongoDB</p>
         </Col>
-        <Col className="col-12 col-md-6 col-lg-4 d-flex justify-content-center align-items-center p-0 mt-3 mb-3">
-          WebSocket
+        <Col className={`${styles.skillCol} col-12 col-md-6 col-lg-4`}>
+          <div className={styles.skillIcon}>
+            <Icon icon={mongodbIcon} className={styles.skillIcon} />
+          </div>
+          <p>Mongoose</p>
         </Col>
-        <Col className="col-12 col-md-6 col-lg-4 d-flex justify-content-center align-items-center p-0 mt-3 mb-3">
-          Mongoose
+        <Col className={`${styles.skillCol} col-12 col-md-6 col-lg-4`}>
+          <div className={styles.skillIcon}>
+            <Icon icon={websocketIcon} fill="#ffffff" />
+          </div>
+          <p>WebSocket</p>
         </Col>
-        <Col className="col-12 col-md-6 col-lg-4 d-flex justify-content-center align-items-center p-0 mt-3 mb-3">
-          Jira
+        <Col className={`${styles.skillCol} col-12 col-md-6 col-lg-4`}>
+          <div className={styles.skillIcon}>
+            <Icon icon={jiraIcon} className={styles.skillIcon} />
+          </div>
+          <p>Jira</p>
         </Col>
       </Row>
       {/* <h2>Methods</h2>
@@ -71,17 +132,29 @@ const Component = ({ className, children }) => {
       </Row> */}
       <h2>Design</h2>
       <Row className={styles.skillRow}>
-        <Col className="col-12 col-md-6 col-lg-4 d-flex justify-content-center align-items-center p-0 mt-3 mb-3">
-          Figma
+        <Col className={`${styles.skillCol} col-12 col-md-6 col-lg-4`}>
+          <div className={styles.skillIcon}>
+            <Icon icon={figmaIcon} className={styles.skillIcon} />
+          </div>
+          <p>Figma</p>
         </Col>
-        <Col className="col-12 col-md-6 col-lg-4 d-flex justify-content-center align-items-center p-0 mt-3 mb-3">
-          Adobe Photoshop
+        <Col className={`${styles.skillCol} col-12 col-md-6 col-lg-4`}>
+          <div className={styles.skillIcon}>
+            <Icon icon={adobephotoshopIcon} className={styles.skillIcon} />
+          </div>
+          <p> Adobe Photoshop</p>
         </Col>
-        <Col className="col-12 col-md-6 col-lg-4 d-flex justify-content-center align-items-center p-0 mt-3 mb-3">
-          Adobe Illustrator
+        <Col className={`${styles.skillCol} col-12 col-md-6 col-lg-4`}>
+          <div className={styles.skillIcon}>
+            <Icon icon={adobeillustratorIcon} className={styles.skillIcon} />
+          </div>
+          <p>Adobe Illustrator</p>
         </Col>
-        <Col className="col-12 col-md-6 col-lg-4 d-flex justify-content-center align-items-center p-0 mt-3 mb-3">
-          Birdfont
+        <Col className={`${styles.skillCol} col-12 col-md-6 col-lg-4`}>
+          <div className={styles.skillIcon}>
+            <Icon icon={writeDotAs} className={styles.skillIcon} />
+          </div>
+          <p>Birdfont</p>
         </Col>
       </Row>
       <main>{children}</main>
